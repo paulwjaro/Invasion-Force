dfd7c4154abac030719cb3d333377acb00b188e7
Paul <paulwjaroslawski@gmail.com>
2021-01-30 12:32:42 -0500

    Added some extra enemy paths.

--- level_data.py
+++ level_data.py
@@ -4,7 +4,11 @@ paths = {
     # Horizontal Zigzags
     '0': [[-64, 32], [10, 32], [110, 96], [210, 32], [310, 96], [410, 32], [510, 96], [610, 32], [664, 32]],
     '1': [[-64, 128], [10, 128], [110, 192], [210, 128], [310, 192], [410, 128], [510, 192], [610, 128], [664, 128]],
-    '2': [[-64, 224], [10, 224], [110, 288], [210, 224], [310, 288], [410, 224], [510, 288], [610, 224], [664, 224]]
+    '2': [[-64, 224], [10, 224], [110, 288], [210, 224], [310, 288], [410, 224], [510, 288], [610, 224], [664, 224]],
+    '3': [[60, -64], [60, 300], [580, 300], [580, -64]],
+    '4': [[-64, 160], [100, 160], [180, 460], [220, 160], [380, 160], [460, 460], [540, 160], [704, 160]],
+    '5': [[20, -64], [20, 20], [620, 340], [20, 340], [620, 20], [620, -64]],
+    '6': [[-64, 300], [20, 300], [320, 60], [620, 300],[704, 300]]
 }
 
 path_system = Paths()
@@ -21,7 +25,7 @@ levels = {
             '0': {
                 'strands': {
                     '0': {
-                        'length': 2,
+                        'length': 10,
                         'enemy': enemies_data['Drone_1'],
                         'path': path_system.path_list[0]
                     }
@@ -30,9 +34,9 @@ levels = {
             '1': {
                 'strands': {
                     '0': {
-                        'length': 3,
+                        'length': 12,
                         'enemy': enemies_data['Drone_2'],
-                        'path': path_system.path_list[1]
+                        'path': path_system.path_list[2]
                     }
                 }
             },
@@ -43,21 +47,45 @@ levels = {
             '0': {
                 'strands': {
                     '0': {
-                        'length': 3,
+                        'length': 15,
                         'enemy': enemies_data['Bot_1'],
-                        'path': path_system.path_list[2]
+                        'path': path_system.path_list[5]
+                    },
+                    '1': {
+                        'length': 15,
+                        'enemy': enemies_data['Drone_2'],
+                        'path': path_system.path_list[1]
                     }
                 }
             },
             '1': {
                 'strands': {
                     '0': {
-                        'length': 4,
+                        'length': 15,
+                        'enemy': enemies_data['Drone_1'],
+                        'path': path_system.path_list[4]
+                    },
+                    '1': {
+                        'length': 15,
                         'enemy': enemies_data['Bot_2'],
-                        'path': path_system.path_list[0]
+                        'path': path_system.path_list[3]
                     }
                 }
             },
+            '2': {
+                'strands': {
+                    '0': {
+                        'length': 12,
+                        'enemy': enemies_data['Bot_1'],
+                        'path': path_system.path_list[6]
+                    },
+                    '1': {
+                        'length': 15,
+                        'enemy': enemies_data['Ship_1'],
+                        'path': path_system.path_list[5]
+                    }
+                }
+            }
         }
     },
     '3': {
@@ -65,22 +93,72 @@ levels = {
             '0': {
                 'strands': {
                     '0': {
-                        'length': 3,
+                        'length': 20,
                         'enemy': enemies_data['Ship_1'],
                         'path': path_system.path_list[2]
+                    },
+                    '1': {
+                        'length': 18,
+                        'enemy': enemies_data['Bot_2'],
+                        'path': path_system.path_list[5]
                     }
                 }
             },
             '1': {
                 'strands': {
                     '0': {
-                        'length': 4,
+                        'length': 18,
                         'enemy': enemies_data['Ship_2'],
-                        'path': path_system.path_list[1]
+                        'path': path_system.path_list[0]
 
+                    },
+                    '1': {
+                        'length': 20,
+                        'enemy': enemies_data['Drone_2'],
+                        'path': path_system.path_list[3]
                     }
                 }
             },
+            '2': {
+                'strands': {
+                    '0': {
+                        'length': 20,
+                        'enemy': enemies_data['Bot_1'],
+                        'path': path_system.path_list[6]
+
+                    },
+                    '1': {
+                        'length': 22,
+                        'enemy': enemies_data['Bot_2'],
+                        'path': path_system.path_list[5]
+                    },
+                    '2': {
+                        'length': 18,
+                        'enemy': enemies_data['Ship_1'],
+                        'path': path_system.path_list[1]
+                    }
+                }
+            },
+            '3': {
+                'strands': {
+                    '0': {
+                        'length': 22,
+                        'enemy': enemies_data['Ship_2'],
+                        'path': path_system.path_list[4]
+
+                    },
+                    '1': {
+                        'length': 24,
+                        'enemy': enemies_data['Drone_1'],
+                        'path': path_system.path_list[2]
+                    },
+                    '2': {
+                        'length': 24,
+                        'enemy': enemies_data['Ship_1'],
+                        'path': path_system.path_list[3]
+                    }
+                }
+            }
         }
     }
 }
